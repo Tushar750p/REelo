ddf5e9eaabe6c5a96bfbac811138f4c7683c3917
Tushar750p <tp991425@gmail.com>
2026-09-08 09:03:05 +0530

    Connect LIVE moderation API

--- backend/app.py
+++ backend/app.py
@@ -14,6 +14,7 @@ from live_api import router as live_router
 from live_ws import router as live_ws_router
 from livekit_api import router as livekit_router
 from live_guests_api import router as live_guests_router
+from live_moderation_api import router as live_moderation_router
 from moderation_api import router as moderation_router
 from creator_api import router as creator_router
 from monetization_api import router as monetization_router
@@ -56,6 +57,7 @@ app.include_router(live_router)
 app.include_router(live_ws_router)
 app.include_router(livekit_router)
 app.include_router(live_guests_router)
+app.include_router(live_moderation_router)
 app.include_router(trending_router)
 app.include_router(topics_router)
 app.include_router(smart_search_router)
