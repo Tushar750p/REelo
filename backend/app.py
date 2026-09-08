d12196f7c40c22174081870b419af45c68d54e94
Tushar750p <tp991425@gmail.com>
2026-09-08 08:49:44 +0530

    Connect LiveKit SFU token API

--- backend/app.py
+++ backend/app.py
@@ -12,6 +12,7 @@ from editor_api import router as editor_router
 from drafts_api import router as drafts_router
 from live_api import router as live_router
 from live_ws import router as live_ws_router
+from livekit_api import router as livekit_router
 from moderation_api import router as moderation_router
 from creator_api import router as creator_router
 from monetization_api import router as monetization_router
@@ -52,6 +53,7 @@ app.include_router(editor_router)
 app.include_router(drafts_router)
 app.include_router(live_router)
 app.include_router(live_ws_router)
+app.include_router(livekit_router)
 app.include_router(trending_router)
 app.include_router(topics_router)
 app.include_router(smart_search_router)
